98a3800e8a037b059c274abb249a8653cc4ccd11
dabeann <dabin4078@daum.net>
2024-05-21 20:22:58 +0900

    feat: enum related to counsel and dialog

---
+++ src/main/java/com/backend/dorandoran/common/domain/CounselorType.java
@@ -0,0 +1,5 @@
+package com.backend.dorandoran.common.domain;
+
+public enum CounselorType {
+    일반
+}
---
+++ src/main/java/com/backend/dorandoran/common/domain/DialogRole.java
@@ -0,0 +1,5 @@
+package com.backend.dorandoran.common.domain;
+
+public enum DialogRole {
+    FROM_CONSULTANT, FROM_USER
+}
