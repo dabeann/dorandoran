2fa4124255e126c826b8ed0e72f4cf27da043701
dajeongdev <dajeongdev@gmail.com>
2024-05-22 16:03:49 +0900

    feat : validation을 위한 어노테이션 및 정규표현식, 메시지 옵션 추가

--- src/main/java/com/backend/dorandoran/user/domain/request/LoginRequest.java
+++ src/main/java/com/backend/dorandoran/user/domain/request/LoginRequest.java
@@ -1,7 +1,14 @@
 package com.backend.dorandoran.user.domain.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record LoginRequest(
+        @NotNull
+        @Pattern(regexp = "^[가-힣]*$", message = "이름은 한글만 가능합니다.")
         String name,
+        @NotNull
+        @Pattern(regexp = "^\\d{11}$", message = "휴대폰번호는 11자리만 가능합니다.")
         String phoneNumber
 ) {
 }
