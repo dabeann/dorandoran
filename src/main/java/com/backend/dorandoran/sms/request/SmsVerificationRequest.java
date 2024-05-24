afe812c4b71ce11420260ccbc1e6ba2159ed9537
dajeongdev <dajeongdev@gmail.com>
2024-05-24 16:59:58 +0900

    fix : 입력값 정규표현식 및 에러 메세지 수정

---
+++ src/main/java/com/backend/dorandoran/sms/request/SmsVerificationRequest.java
@@ -0,0 +1,14 @@
+package com.backend.dorandoran.sms.request;
+
+import jakarta.validation.constraints.Pattern;
+import org.jetbrains.annotations.NotNull;
+
+public record SmsVerificationRequest(
+        @NotNull
+        @Pattern(regexp = "^[0-9]{11}$", message = "휴대폰번호 11자리를 입력해주세요.")
+        String phoneNumber,
+        @NotNull
+        @Pattern(regexp = "^[0-9]{6}$", message = "인증번호 6자리를 입력해주세요.")
+        String verificationCode
+) {
+}
--- src/main/java/com/backend/dorandoran/user/domain/request/LoginRequest.java
+++ src/main/java/com/backend/dorandoran/user/domain/request/LoginRequest.java
@@ -8,7 +8,7 @@ public record LoginRequest(
         @Pattern(regexp = "^[가-힣]*$", message = "이름은 한글만 가능합니다.")
         String name,
         @NotNull
-        @Pattern(regexp = "^\\d{11}$", message = "휴대폰번호는 11자리만 가능합니다.")
+        @Pattern(regexp = "^[0-9]{11}$", message = "휴대폰번호 11자리를 입력해주세요.")
         String phoneNumber
 ) {
 }
