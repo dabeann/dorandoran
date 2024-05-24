b77ebe39df1ca43a1b8e6506d9b6d6f3b26b8d13
dajeongdev <dajeongdev@gmail.com>
2024-05-24 17:00:19 +0900

    fix : ValidationError 리턴 형식 수정

--- src/main/java/com/backend/dorandoran/common/handler/ValidationExceptionHandler.java
+++ src/main/java/com/backend/dorandoran/common/handler/ValidationExceptionHandler.java
@@ -5,25 +5,20 @@ import com.backend.dorandoran.common.exception.CommonException;
 import com.backend.dorandoran.common.response.CommonResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 class ValidationExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<CommonResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String field = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(field, errorMessage);
-        });
+    ResponseEntity<CommonResponse<List<String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> errors.add(error.getDefaultMessage()));
         return new ResponseEntity<>(new CommonResponse<>(ErrorCode.INVALID_REQUEST.getErrorMessage(), errors), HttpStatus.BAD_REQUEST);
     }
 
