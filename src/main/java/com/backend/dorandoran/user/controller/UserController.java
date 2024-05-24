9f0d90b2aefe01147ee2179cd9971f187713a356
dajeongdev <dajeongdev@gmail.com>
2024-05-24 17:33:13 +0900

    feat : Swagger 설정 추가

--- src/main/java/com/backend/dorandoran/user/controller/UserController.java
+++ src/main/java/com/backend/dorandoran/user/controller/UserController.java
@@ -3,9 +3,9 @@ package com.backend.dorandoran.user.controller;
 import com.backend.dorandoran.common.domain.response.BasicApiSwaggerResponse;
 import com.backend.dorandoran.common.domain.response.CommonResponse;
 import com.backend.dorandoran.security.jwt.service.JwtUtil;
-import com.backend.dorandoran.sms.request.SmsCertificationRequest;
-import com.backend.dorandoran.sms.service.SmsCertificationService;
 import com.backend.dorandoran.user.domain.request.LoginRequest;
+import com.backend.dorandoran.user.domain.request.SmsSendRequest;
+import com.backend.dorandoran.user.domain.request.SmsVerificationRequest;
 import com.backend.dorandoran.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -17,7 +17,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User", description = "사용자 관련 API입니다.")
 @RequestMapping("/api/user")
@@ -27,7 +30,37 @@ class UserController {
 
     private final UserService userService;
     private final JwtUtil jwtUtil;
-    private final SmsCertificationService smsCertificationService;
+
+    @Operation(summary = "summary : SMS 인증번호 전송",
+            description = """
+                    ## 요청 :
+                    - String phoneNumber (숫자 11자리만 가능) (필수)
+                    ## 응답 :
+                    - 200, "Success"
+                    """)
+    @BasicApiSwaggerResponse
+    @ApiResponse(responseCode = "200")
+    @PostMapping("/send-sms")
+    ResponseEntity<CommonResponse<String>> sendSms(@Valid @RequestBody SmsSendRequest smsSendRequest) {
+        userService.sendSms(smsSendRequest);
+        return new ResponseEntity<>(new CommonResponse<>("SMS 인증번호 전송", "success"), HttpStatus.OK);
+    }
+
+    @Operation(summary = "summary : SMS 인증번호 확인",
+            description = """
+                    ## 요청 :
+                    - String phoneNumber (숫자 11자리만 가능) (필수)
+                    - String verificationCode (숫자 6자리만 가능) (필수)
+                    ## 응답 :
+                    - 200, "Success"
+                    """)
+    @BasicApiSwaggerResponse
+    @ApiResponse(responseCode = "200")
+    @PostMapping("/confirm-sms")
+    ResponseEntity<CommonResponse<String>> smsVerification(@Valid @RequestBody SmsVerificationRequest smsVerificationRequest) {
+        userService.verifySms(smsVerificationRequest);
+        return new ResponseEntity<>(new CommonResponse<>("SMS 인증번호 확인", "Success"), HttpStatus.OK);
+    }
 
     @Operation(summary = "summary : 회원가입/로그인",
             description = """
@@ -41,24 +74,12 @@ class UserController {
     @BasicApiSwaggerResponse
     @ApiResponse(responseCode = "200")
     @PostMapping("/login")
-    ResponseEntity<CommonResponse<Boolean>> login(@Valid @RequestBody LoginRequest request) {
+    ResponseEntity<CommonResponse<String>> login(@Valid @RequestBody LoginRequest request) {
         HttpHeaders headers = new HttpHeaders();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         headers.add(HttpHeaders.AUTHORIZATION, !authentication.isAuthenticated() ?
                 userService.joinOrLogin(request) : jwtUtil.createAccessToken(authentication));
-        // TODO 심리검사 여부 추가
-        return new ResponseEntity<>(new CommonResponse<>("회원가입/로그인", null), headers, HttpStatus.OK);
-    }
-
-    @PostMapping("/send-sms")
-    ResponseEntity<CommonResponse<String>> sendSms(@RequestParam("phoneNumber") String phoneNumber) {
-        smsCertificationService.sendSms(phoneNumber);
-        return new ResponseEntity<>(new CommonResponse<>("SMS 전송", "success"), HttpStatus.OK);
-    }
-
-    @PostMapping("/confirm-sms")
-    ResponseEntity<CommonResponse<Boolean>> smsVerification(@RequestBody SmsCertificationRequest request) {
-        smsCertificationService.verifySms(request);
-        return new ResponseEntity<>(new CommonResponse<>("SMS 인증번호 확인", true), HttpStatus.OK);
+        // TODO "Success" 대신 심리검사 여부 추가
+        return new ResponseEntity<>(new CommonResponse<>("SMS 인증번호 전송", "Success"), headers, HttpStatus.OK);
     }
 }
