a90efa2b1daab99e4c74cf088c8694957046fd1b
dajeongdev <dajeongdev@gmail.com>
2024-05-24 23:05:08 +0900

    feat : 토큰 발급 로직 분기 및 코드 분리

--- src/main/java/com/backend/dorandoran/user/service/UserService.java
+++ src/main/java/com/backend/dorandoran/user/service/UserService.java
@@ -3,19 +3,21 @@ package com.backend.dorandoran.user.service;
 import com.backend.dorandoran.common.domain.ErrorCode;
 import com.backend.dorandoran.common.exception.CommonException;
 import com.backend.dorandoran.security.jwt.service.JwtUtil;
-import com.backend.dorandoran.user.domain.request.SmsSendRequest;
-import com.backend.dorandoran.user.repository.SmsVerificationRepository;
-import com.backend.dorandoran.user.domain.request.SmsVerificationRequest;
 import com.backend.dorandoran.user.domain.entity.User;
-import com.backend.dorandoran.user.domain.request.LoginRequest;
+import com.backend.dorandoran.user.domain.entity.UserToken;
+import com.backend.dorandoran.user.domain.request.SmsSendRequest;
+import com.backend.dorandoran.user.domain.request.SmsVerificationRequest;
+import com.backend.dorandoran.user.repository.SmsVerificationRepository;
 import com.backend.dorandoran.user.repository.UserRepository;
+import com.backend.dorandoran.user.repository.querydsl.UserQueryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.Optional;
 
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -24,36 +26,62 @@ public class UserService {
     private final JwtUtil jwtUtil;
     private final SmsUtil smsUtil;
     private final SmsVerificationRepository smsVerificationRepository;
+    private final UserTokenService userTokenService;
+    private final UserQueryRepository userQueryRepository;
 
-    @Transactional
-    public String joinOrLogin(LoginRequest request) {
-        Optional<User> userOptional = userRepository.findByNameAndPhoneNumber(request.name(), request.phoneNumber());
-        User user;
-        if (userOptional.isPresent()) { // 로그인
-            user = userOptional.get();
-            if (!request.name().equals(user.getName())) {
-                throw new CommonException(ErrorCode.NOT_FOUND_USERNAME);
-            }
-        } else { // 회원가입
-            user = User.toUserEntity(request);
-            userRepository.save(user);
-        }
-        return jwtUtil.saveToken(user.getId());
-    }
-
+    @Transactional(readOnly = true)
     public void sendSms(SmsSendRequest request) {
+        compareNameAndPhoneNumber(request);
+
         String verificationCode = String.valueOf((int) (Math.random() * 900000) + 100000);
         smsUtil.sendSms(request.phoneNumber(), verificationCode);
         smsVerificationRepository.createSmsVerificationCode(request.phoneNumber(), verificationCode);
     }
 
-    public void verifySms(SmsVerificationRequest request) {
+    private void compareNameAndPhoneNumber(SmsSendRequest request) {
+        Optional<User> userOptional = userRepository.findByNameAndPhoneNumber(request.name(), request.phoneNumber());
+
+        if (userOptional.isPresent() && !request.name().equals(userOptional.get().getName())) {
+            throw new CommonException(ErrorCode.NOT_FOUND_USERNAME);
+        }
+    }
+
+    public String verifySms(SmsVerificationRequest request) {
         String redisVerificationCode = smsVerificationRepository.getSmsVerificationCode(request.phoneNumber());
-        if (smsVerificationRepository.hasKey(request.phoneNumber())) {
+        if (!smsVerificationRepository.hasKey(request.phoneNumber()) || !StringUtils.hasText(redisVerificationCode)) {
             throw new CommonException(ErrorCode.EXPIRED_AUTH_CODE);
         } else if (!redisVerificationCode.equals(request.verificationCode())) {
             throw new CommonException(ErrorCode.WRONG_AUTH_CODE);
         }
         smsVerificationRepository.removeSmsVerificationCode(request.phoneNumber());
+
+        return joinOrLogin(request);
+    }
+
+    @Transactional
+    public String joinOrLogin(SmsVerificationRequest request) {
+        Optional<UserToken> userOptional = userQueryRepository.findUserTokenByPhoneNumber(request.phoneNumber());
+        return userOptional.isPresent() ? updateUserToken(userOptional.get()) : saveUserToken(request);
+    }
+
+    private String updateUserToken(UserToken userToken) {
+        Authentication authentication = jwtUtil.getAuthenticationByUserId(userToken.getUserId());
+
+        String refreshToken = jwtUtil.createRefreshToken(authentication);
+        userToken.updateRefreshToken(refreshToken);
+        userTokenService.save(userToken); // TODO 더티체킹으로 처리하는 방법 찾기(쿼리 줄이기)
+        return jwtUtil.createAccessToken(authentication);
+    }
+
+    private String saveUserToken(SmsVerificationRequest request) {
+        User user = User.toUserEntity(request);
+        userRepository.save(user);
+
+        Authentication authentication = jwtUtil.getAuthenticationByUserId(user.getId());
+
+        String refreshToken = jwtUtil.createRefreshToken(authentication);
+        UserToken userToken = UserToken.toUserTokenEntity(user.getId(), refreshToken);
+        userTokenService.save(userToken);
+        return jwtUtil.createAccessToken(authentication);
     }
 }
