050870b76f2e9c04cb256f494e7ace5ac13d5a5f
dajeongdev <dajeongdev@gmail.com>
2024-05-24 16:29:33 +0900

    fix : 필요없는 리턴값 제거

--- src/main/java/com/backend/dorandoran/sms/service/SmsUtil.java
+++ src/main/java/com/backend/dorandoran/sms/service/SmsUtil.java
@@ -4,7 +4,6 @@ import jakarta.annotation.PostConstruct;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
-import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -27,15 +26,15 @@ public class SmsUtil {
 
     @PostConstruct
     private void init() {
-        this.messageService = NurigoApp.INSTANCE.initialize(API_KEY, API_SECRET_KEY, "https://api.coolsms.co.kr");
+        messageService = NurigoApp.INSTANCE.initialize(API_KEY, API_SECRET_KEY, "https://api.coolsms.co.kr");
     }
 
-    public SingleMessageSentResponse sendSms(String phoneNumber, String verificationCode) {
+    public void sendSms(String phoneNumber, String verificationCode) {
         Message message = new Message();
         message.setFrom(FROM_PHONE_NUMBER);
         message.setTo(phoneNumber);
         message.setText("[도란도란] 인증번호[" + verificationCode + "] 타인에게 절대 알려주지 마세요.");
 
-        return this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        messageService.sendOne(new SingleMessageSendingRequest(message));
     }
 }
