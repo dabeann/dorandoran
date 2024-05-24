4fd73598cc17275727d805e9b0e01efb4985a5b5
dajeongdev <dajeongdev@gmail.com>
2024-05-24 23:02:20 +0900

    feat : SMS 인증과 로그인 분기점으로 분리

--- src/main/java/com/backend/dorandoran/user/controller/UserController.java
+++ src/main/java/com/backend/dorandoran/user/controller/UserController.java
@@ -2,8 +2,6 @@ package com.backend.dorandoran.user.controller;
 
 import com.backend.dorandoran.common.domain.response.BasicApiSwaggerResponse;
 import com.backend.dorandoran.common.domain.response.CommonResponse;
-import com.backend.dorandoran.security.jwt.service.JwtUtil;
-import com.backend.dorandoran.user.domain.request.LoginRequest;
 import com.backend.dorandoran.user.domain.request.SmsSendRequest;
 import com.backend.dorandoran.user.domain.request.SmsVerificationRequest;
 import com.backend.dorandoran.user.service.UserService;
@@ -15,8 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,11 +25,11 @@ import org.springframework.web.bind.annotation.RestController;
 class UserController {
 
     private final UserService userService;
-    private final JwtUtil jwtUtil;
 
     @Operation(summary = "summary : SMS 인증번호 전송",
             description = """
                     ## 요청 :
+                    - String name (한글만 가능) (필수)
                     - String phoneNumber (숫자 11자리만 가능) (필수)
                     ## 응답 :
                     - 200, "Success"
@@ -43,23 +39,7 @@ class UserController {
     @PostMapping("/send-sms")
     ResponseEntity<CommonResponse<String>> sendSms(@Valid @RequestBody SmsSendRequest smsSendRequest) {
         userService.sendSms(smsSendRequest);
-        return new ResponseEntity<>(new CommonResponse<>("SMS 인증번호 전송", "success"), HttpStatus.OK);
-    }
-
-    @Operation(summary = "summary : SMS 인증번호 확인",
-            description = """
-                    ## 요청 :
-                    - String phoneNumber (숫자 11자리만 가능) (필수)
-                    - String verificationCode (숫자 6자리만 가능) (필수)
-                    ## 응답 :
-                    - 200, "Success"
-                    """)
-    @BasicApiSwaggerResponse
-    @ApiResponse(responseCode = "200")
-    @PostMapping("/confirm-sms")
-    ResponseEntity<CommonResponse<String>> smsVerification(@Valid @RequestBody SmsVerificationRequest smsVerificationRequest) {
-        userService.verifySms(smsVerificationRequest);
-        return new ResponseEntity<>(new CommonResponse<>("SMS 인증번호 확인", "Success"), HttpStatus.OK);
+        return new ResponseEntity<>(new CommonResponse<>("SMS 인증번호 전송", "Success"), HttpStatus.OK);
     }
 
     @Operation(summary = "summary : 회원가입/로그인",
@@ -67,6 +47,7 @@ class UserController {
                     ## 요청 :
                     - String name (한글만 가능) (필수)
                     - String phoneNumber (숫자 11자리만 가능) (필수)
+                    - String verificationCode (숫자 6자리만 가능) (필수)
                     ## 응답 :
                     - header(Authorization Bearer 토큰)
                     - 심리검사 진행 여부(boolean) (추가 예정)
@@ -74,12 +55,11 @@ class UserController {
     @BasicApiSwaggerResponse
     @ApiResponse(responseCode = "200")
     @PostMapping("/login")
-    ResponseEntity<CommonResponse<String>> login(@Valid @RequestBody LoginRequest request) {
+    ResponseEntity<CommonResponse<String>> verifyCodeAndLogin(@Valid @RequestBody SmsVerificationRequest request) {
         HttpHeaders headers = new HttpHeaders();
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        headers.add(HttpHeaders.AUTHORIZATION, !authentication.isAuthenticated() ?
-                userService.joinOrLogin(request) : jwtUtil.createAccessToken(authentication));
+        headers.add(HttpHeaders.AUTHORIZATION, userService.verifySms(request));
+
         // TODO "Success" 대신 심리검사 여부 추가
-        return new ResponseEntity<>(new CommonResponse<>("SMS 인증번호 전송", "Success"), headers, HttpStatus.OK);
+        return new ResponseEntity<>(new CommonResponse<>("회원가입/로그인", "Success"), headers, HttpStatus.OK);
     }
 }
--- src/main/java/com/backend/dorandoran/user/domain/request/SmsVerificationRequest.java
+++ src/main/java/com/backend/dorandoran/user/domain/request/SmsVerificationRequest.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.Pattern;
 import org.jetbrains.annotations.NotNull;
 
 public record SmsVerificationRequest(
+        @jakarta.validation.constraints.NotNull
+        @Pattern(regexp = "^[가-힣]*$", message = "이름은 한글만 가능합니다.")
+        String name,
         @NotNull
         @Pattern(regexp = "^[0-9]{11}$", message = "휴대폰번호 11자리를 입력해주세요.")
         String phoneNumber,
