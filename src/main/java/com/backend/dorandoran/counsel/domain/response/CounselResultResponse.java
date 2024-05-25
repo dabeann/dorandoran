9d1508354be589f4e320c5d2ed0541374b4f3a6b
dabeann <dabin4078@daum.net>
2024-05-25 13:28:41 +0900

    feat: counsel result response

---
+++ src/main/java/com/backend/dorandoran/counsel/domain/response/CounselResultResponse.java
@@ -0,0 +1,21 @@
+package com.backend.dorandoran.counsel.domain.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CounselResultResponse {
+    private String result;
+    private String summary;
+    private List<CounselResultPsychotherapyContents> contents;
+
+    @Getter
+    @AllArgsConstructor
+    public static class CounselResultPsychotherapyContents {
+        private String title;
+        private String link;
+        private String thumbnailLink;
+    }
+}
