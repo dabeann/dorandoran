b22b1286bd62f6d18e2282e590e5b99a73da9bd9
dabeann <dabin4078@daum.net>
2024-05-25 13:25:15 +0900

    feat: psychotherapy content repository

---
+++ src/main/java/com/backend/dorandoran/contents/repository/PsychotherapyContentsRepository.java
@@ -0,0 +1,7 @@
+package com.backend.dorandoran.contents.repository;
+
+import com.backend.dorandoran.contents.domain.entity.PsychotherapyContents;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PsychotherapyContentsRepository extends JpaRepository<PsychotherapyContents, Long> {
+}
