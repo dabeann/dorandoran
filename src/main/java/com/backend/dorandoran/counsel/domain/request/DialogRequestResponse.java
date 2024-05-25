68ab007fc4f6ead6a041fa32bb7a3a30ebb65180
dabeann <dabin4078@daum.net>
2024-05-25 15:11:06 +0900

    feat: rename request

--- src/main/java/com/backend/dorandoran/counsel/domain/request/DialogRequestResponse.java
+++ src/main/java/com/backend/dorandoran/counsel/domain/request/DialogRequestResponse.java
@@ -1,7 +1,7 @@
 package com.backend.dorandoran.counsel.domain.request;
 
-public record DialogRequest(
-        String counselId,
+public record DialogRequestResponse(
+        Long counselId,
         String message
 ) {
 }
