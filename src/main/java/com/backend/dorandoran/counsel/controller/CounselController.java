424a277926566f9424cb7a5443f88a7e55ad9e41
dabeann <dabin4078@daum.net>
2024-05-25 17:23:28 +0900

    feat: counsel controller with swagger

--- src/main/java/com/backend/dorandoran/counsel/controller/CounselController.java
+++ src/main/java/com/backend/dorandoran/counsel/controller/CounselController.java
@@ -1,40 +1,57 @@
 package com.backend.dorandoran.counsel.controller;
 
+import com.backend.dorandoran.common.domain.response.BasicApiSwaggerResponse;
 import com.backend.dorandoran.common.domain.response.CommonResponse;
-import com.backend.dorandoran.counsel.domain.request.DialogRequest;
+import com.backend.dorandoran.counsel.domain.request.DialogRequestResponse;
+import com.backend.dorandoran.counsel.domain.response.CounselResultResponse;
+import com.backend.dorandoran.counsel.service.CounselService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+@Tag(name = "Counsel", description = "상담 관련 API입니다.")
 @RequestMapping("/api/counsel")
 @RequiredArgsConstructor
 @RestController
 class CounselController {
 
+    private final CounselService counselService;
+
+    @Operation(summary = "summary : 상담 채팅",
+            description = """
+                    ## 요청 :
+                    - Long counselId (필수)
+                    - String message (필수)
+                    ## 응답 :
+                    - String "gpt 상담자의 답변"
+                    """)
+    @BasicApiSwaggerResponse
+    @ApiResponse(responseCode = "200")
     @PostMapping("/chat")
-    ResponseEntity<CommonResponse<String>> getChatResult(@RequestBody DialogRequest request) {
-        String counselId = request.counselId();
+    ResponseEntity<CommonResponse<String>> getChatResult(@RequestBody DialogRequestResponse request) {
+        String counselId = String.valueOf(request.counselId());
         String userMessage = request.message();
 
         try {
-            log.info("Python 코드 실행 중...");
-
             ProcessBuilder processBuilder = new ProcessBuilder("python",
-                    "src/main/java/com/backend/dorandoran/counsel/service/langchain/LangChainEntireDialog.py",
+                    "src/main/java/com/backend/dorandoran/counsel/service/python/LangChainEntireDialog.py",
                     counselId, userMessage);
             processBuilder.redirectErrorStream(true);
 
             Process process = processBuilder.start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
             StringBuilder output = new StringBuilder();
             String line;
 
@@ -45,18 +62,78 @@ class CounselController {
 
             int exitCode = process.waitFor();
             if (exitCode != 0) {
-                log.error("Python 스크립트 실행 중 오류 발생. 종료 코드: " + exitCode);
-                log.error("Python 오류 메시지: " + output.toString().trim());
                 return new ResponseEntity<>(
                         new CommonResponse<>("Error: Python script execution failed with exit code ",
                                 exitCode + "\n" + output.toString().trim()), HttpStatus.BAD_REQUEST);
             }
 
             String result = output.toString().trim();
-            return new ResponseEntity<>(new CommonResponse<>("채팅", result), HttpStatus.OK);
+            return new ResponseEntity<>(new CommonResponse<>("상담 채팅", result), HttpStatus.OK);
         } catch (Exception e) {
-            log.error("Python 스크립트 호출 중 오류 발생: ", e);
-            return new ResponseEntity<>(new CommonResponse<>("Error: ", e.getMessage()), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new CommonResponse<>("Error: ", e.toString()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Operation(summary = "summary : 상담 시작",
+            description = """
+                    ## 요청 :
+                    - 토큰 (필수)
+                    ## 응답 :
+                    - Long counselId
+                    - String message
+                    """)
+    @BasicApiSwaggerResponse
+    @ApiResponse(responseCode = "200")
+    @PostMapping("/start")
+    ResponseEntity<CommonResponse<DialogRequestResponse>> startCounsel() {
+        return new ResponseEntity<>(new CommonResponse<>("상담 시작", counselService.startCounsel()), HttpStatus.OK);
+    }
+
+    @Operation(summary = "summary : 상담 종료",
+            description = """
+                    ## 요청 :
+                    - 토큰 (필수)
+                    - {counselId} (필수)
+                    ## 응답 :
+                    - String result
+                    - String summary
+                    - contents : [{String title, String link, String thumbnailLink}, ...]
+                    """)
+    @BasicApiSwaggerResponse
+    @ApiResponse(responseCode = "200")
+    @PostMapping("/end/{counselId}")
+    ResponseEntity<CommonResponse<CounselResultResponse>> endCounsel(@PathVariable("counselId") Long counselId) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("python",
+                    "src/main/java/com/backend/dorandoran/counsel/service/python/EndCounsel.py",
+                    String.valueOf(counselId));
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+            StringBuilder output = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                output.append(line);
+                output.append(System.lineSeparator());
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                return new ResponseEntity<>(
+                        new CommonResponse<>("Error: Python script execution failed with exit code "
+                                + exitCode + "\n" + output.toString().trim(), null), HttpStatus.BAD_REQUEST);
+            }
+
+            String summary = output.toString().trim();
+
+            return new ResponseEntity<>(new CommonResponse<>("상담 결과",
+                    counselService.endCounsel(counselId, summary)), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new CommonResponse<>("Error: " + e.toString(), null),
+                    HttpStatus.BAD_REQUEST);
         }
     }
 }
