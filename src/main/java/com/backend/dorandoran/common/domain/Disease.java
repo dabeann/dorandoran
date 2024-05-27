bc063480dd7e5928e127b432afb05c63a18c4e11
dabeann <dabin4078@daum.net>
2024-05-27 17:51:40 +0900

    feat: add priority

--- src/main/java/com/backend/dorandoran/common/domain/Disease.java
+++ src/main/java/com/backend/dorandoran/common/domain/Disease.java
@@ -1,5 +1,19 @@
 package com.backend.dorandoran.common.domain;
 
 public enum Disease {
-    DEPRESSION, STRESS_OVERLOAD, ANXIETY, ALCOHOLISM, SMOKING_ADDICTION
+    DEPRESSION(1),
+    STRESS_OVERLOAD(2),
+    ANXIETY(3),
+    ALCOHOLISM(4),
+    SMOKING_ADDICTION(5);
+
+    private final int priority;
+
+    Disease(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
