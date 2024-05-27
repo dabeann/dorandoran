82a448a86333df829dfcec248a97be4e7f0c9f3c
dabeann <dabin4078@daum.net>
2024-05-27 15:21:38 +0900

    feat: contents service

---
+++ src/main/java/com/backend/dorandoran/contents/service/ContentsService.java
@@ -0,0 +1,11 @@
+package com.backend.dorandoran.contents.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ContentsService {
+
+
+}
