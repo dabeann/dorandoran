b245e1e06ad36f89c8503ad189bd04da485b2440
dabeann <dabin4078@daum.net>
2024-05-29 14:25:49 +0900

    feat: main contents method with psychological assessment status

--- src/main/java/com/backend/dorandoran/contents/service/ContentsService.java
+++ src/main/java/com/backend/dorandoran/contents/service/ContentsService.java
@@ -4,6 +4,7 @@ import com.backend.dorandoran.common.domain.Disease;
 import com.backend.dorandoran.common.domain.ErrorCode;
 import com.backend.dorandoran.common.validator.CommonValidator;
 import com.backend.dorandoran.contents.domain.entity.PsychotherapyContents;
+import com.backend.dorandoran.contents.domain.entity.Quotation;
 import com.backend.dorandoran.contents.domain.response.ContentsResponse;
 import com.backend.dorandoran.contents.repository.PsychotherapyContentsRepository;
 import com.backend.dorandoran.contents.repository.QuotationRepository;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -25,25 +27,43 @@ public class ContentsService {
     private final QuotationRepository quotationRepository;
     private final PsychotherapyContentsRepository psychotherapyContentsRepository;
 
+    @Transactional
+    public void updateTodayQuotation() {
+        List<User> usersWithDiseases = userRepository.findAllByDiseasesNotNull();
+        for (User user : usersWithDiseases) {
+            List<Quotation> quotationsByDiseases = quotationRepository.findAllByCategoryIn(Arrays.stream(user.getDiseases())
+                    .map(Disease::valueOf)
+                    .toList());
+            Collections.shuffle(quotationsByDiseases);
+            user.updateTodayQuotation(quotationsByDiseases.stream().findFirst().get());
+        }
+    }
+
     public ContentsResponse getMainContents(String category) {
         final Long userId = UserInfoUtil.getUserIdOrThrow();
         Optional<User> findUser = userRepository.findById(userId);
         User user = findUser.get();
 
-        List<Disease> diseasesList = Arrays.stream(user.getDiseases())
-                .map(Disease::valueOf)
-                .toList();
+        String quotation = null;
+        if (user.getDiseases() != null) {
+            quotation = user.getTodayQuotation().getContents();
+        }
 
         List<PsychotherapyContents> psychotherapyContentsList;
-        if (category == null) {
-            // null이면 "당신을 위한 콘텐츠"
+        if (user.getDiseases() == null && category == null) {
+            // 심리검사 X
+            psychotherapyContentsList = getPsychotherapyContentsListWithCategory("우울증");
+        } else if (category == null) {
+            // "당신을 위한 콘텐츠"
+            List<Disease> diseasesList = Arrays.stream(user.getDiseases())
+                    .map(Disease::valueOf)
+                    .toList();
             psychotherapyContentsList = getPersonalizedPsychotherapyContentsList(diseasesList);
         } else {
             psychotherapyContentsList = getPsychotherapyContentsListWithCategory(category);
         }
 
-        // TODO 하루에 하나 병에 따라 랜덤 출력
-        return new ContentsResponse("quotation", psychotherapyContentsList);
+        return new ContentsResponse(quotation, psychotherapyContentsList);
     }
 
     private List<PsychotherapyContents> getPsychotherapyContentsListWithCategory(String category) {
