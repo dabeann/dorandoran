cb97eaae582702d4aa41e1c52e60d036e5857a40
dabeann <dabin4078@daum.net>
2024-05-29 14:23:40 +0900

    feat: add method in quotation repository

--- src/main/java/com/backend/dorandoran/contents/repository/QuotationRepository.java
+++ src/main/java/com/backend/dorandoran/contents/repository/QuotationRepository.java
@@ -1,7 +1,11 @@
 package com.backend.dorandoran.contents.repository;
 
+import com.backend.dorandoran.common.domain.Disease;
 import com.backend.dorandoran.contents.domain.entity.Quotation;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuotationRepository extends JpaRepository<Quotation, Long> {
+
+    List<Quotation> findAllByCategoryIn(List<Disease> categories);
 }
