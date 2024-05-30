3846f3f53e13d5270db28d063129d9d52a8ffb97
dabeann <dabin4078@daum.net>
2024-05-30 15:21:54 +0900

    feat: add find first meditation content by duration method

--- src/main/java/com/backend/dorandoran/contents/repository/MeditationContentsRepository.java
+++ src/main/java/com/backend/dorandoran/contents/repository/MeditationContentsRepository.java
@@ -1,7 +1,9 @@
 package com.backend.dorandoran.contents.repository;
 
+import com.backend.dorandoran.common.domain.MeditationDuration;
 import com.backend.dorandoran.contents.domain.entity.MeditationContents;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MeditationContentsRepository extends JpaRepository<MeditationContents, Long> {
+    MeditationContents findFirstByDurationCategory(MeditationDuration duration);
 }
