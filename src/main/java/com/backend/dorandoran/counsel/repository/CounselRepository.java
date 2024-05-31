6ef03f7b58e9cd1f6f212b9edcbcc315404fa959
dabeann <dabin4078@daum.net>
2024-05-31 13:39:18 +0900

    feat: add method in counsel repository

--- src/main/java/com/backend/dorandoran/counsel/repository/CounselRepository.java
+++ src/main/java/com/backend/dorandoran/counsel/repository/CounselRepository.java
@@ -1,9 +1,13 @@
 package com.backend.dorandoran.counsel.repository;
 
+import com.backend.dorandoran.common.domain.counsel.CounselState;
 import com.backend.dorandoran.counsel.domain.entity.Counsel;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CounselRepository extends JpaRepository<Counsel, Long> {
+
+    List<Counsel> findAllByStateOrderByCreatedDateTimeDesc(CounselState state);
 }
