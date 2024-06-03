5e5e9e13d5471965e40103843d47f2a049e2a7ae
dabeann <dabin4078@daum.net>
2024-06-03 20:45:41 +0900

    feat: counsel history repository

--- src/main/java/com/backend/dorandoran/counsel/repository/CounselRepository.java
+++ src/main/java/com/backend/dorandoran/counsel/repository/CounselRepository.java
@@ -2,6 +2,7 @@ package com.backend.dorandoran.counsel.repository;
 
 import com.backend.dorandoran.common.domain.counsel.CounselState;
 import com.backend.dorandoran.counsel.domain.entity.Counsel;
+import com.backend.dorandoran.user.domain.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CounselRepository extends JpaRepository<Counsel, Long> {
 
-    List<Counsel> findAllByStateOrderByCreatedDateTimeDesc(CounselState state);
+    List<Counsel> findAllByStateAndUserOrderByCreatedDateTimeDesc(CounselState state, User user);
+
+    List<Counsel> findAllByUser(User user);
 }
