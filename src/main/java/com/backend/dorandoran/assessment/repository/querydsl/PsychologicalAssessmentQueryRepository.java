8e55b7dea69fcb6b9d7ba2b3bea1608c833d51c8
dajeongdev <dajeongdev@gmail.com>
2024-06-04 20:24:40 +0900

    fix : 사용하지 않는 코드 삭제

--- src/main/java/com/backend/dorandoran/assessment/repository/querydsl/PsychologicalAssessmentQueryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backend.dorandoran.assessment.repository.querydsl;
-
-public interface PsychologicalAssessmentQueryRepository {
-
-    Boolean hasPsychologicalAssessment(Long userId);
-}
