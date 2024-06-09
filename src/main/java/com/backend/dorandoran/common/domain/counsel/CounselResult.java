973d18c4af5fd06aa44a82fe0c0a1bb91853c776
dabeann <dabin4078@daum.net>
2024-06-09 13:08:55 +0900

    feat: counsel result comment

--- src/main/java/com/backend/dorandoran/common/domain/counsel/CounselResult.java
+++ src/main/java/com/backend/dorandoran/common/domain/counsel/CounselResult.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 
 public enum CounselResult {
     GOOD("님의 심리상태가 더 좋아졌어요!"),
-    BAD("님, 더 노력해봐요!");
+    BAD("님, 오늘 안 좋은 일이 있었나요? 우리 조금만 더 마음 편하게 가져봐요.");
 
     private final String koreanResult;
 
