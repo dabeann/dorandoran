4a48083d2c0afd1561ec9577bbc57289f9d4e9f9
dajeongdev <dajeongdev@gmail.com>
2024-06-15 19:03:27 +0900

    fix : integer validation 수정

--- src/main/java/com/backend/dorandoran/mypage/domain/request/PsychologicalChangeTrendRequest.java
+++ src/main/java/com/backend/dorandoran/mypage/domain/request/PsychologicalChangeTrendRequest.java
@@ -1,14 +1,14 @@
 package com.backend.dorandoran.mypage.domain.request;
 
 import com.backend.dorandoran.common.domain.PsychologicalAssessmentCategory;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Digits;
 import org.jetbrains.annotations.NotNull;
 
 public record PsychologicalChangeTrendRequest(
         @NotNull
         PsychologicalAssessmentCategory category,
         @NotNull
-        @Pattern(regexp = "^[0-9]$", message = "요청월은 숫자만 가능합니다.")
+        @Digits(integer = 2, fraction = 0, message = "숫자만 입력 가능합니다.")
         Integer month
 ) {
 }
