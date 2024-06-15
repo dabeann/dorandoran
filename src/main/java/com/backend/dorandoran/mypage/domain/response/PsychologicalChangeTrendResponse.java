ca73d6c1a7c38d88eea8439c067f3f2a0481d9cc
dajeongdev <dajeongdev@gmail.com>
2024-06-15 19:03:16 +0900

    fix : 완료한 상담 목록 조회 분리로 인한 필드 삭제

--- src/main/java/com/backend/dorandoran/mypage/domain/response/PsychologicalChangeTrendResponse.java
+++ src/main/java/com/backend/dorandoran/mypage/domain/response/PsychologicalChangeTrendResponse.java
@@ -2,7 +2,6 @@ package com.backend.dorandoran.mypage.domain.response;
 
 public record PsychologicalChangeTrendResponse(
         Integer dayOfMonth,
-        Integer score,
-        Long counselId
+        Integer score
 ) {
 }
