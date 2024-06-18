adf0da8f131d88038fe51b78cb53ff1b50070594
dabeann <dabin4078@daum.net>
2024-06-18 19:59:19 +0900

    feat: add message date

--- src/main/java/com/backend/dorandoran/counsel/domain/response/DialogHistory.java
+++ src/main/java/com/backend/dorandoran/counsel/domain/response/DialogHistory.java
@@ -1,6 +1,7 @@
 package com.backend.dorandoran.counsel.domain.response;
 
 import com.backend.dorandoran.counsel.domain.entity.Dialog;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,11 +10,13 @@ import lombok.Getter;
 public class DialogHistory {
     private String role;
     private String message;
+    private LocalDate date;
 
     public DialogHistory(Dialog dialog) {
         this(
                 dialog.getRole().getKoreanName(),
-                dialog.getContents()
+                dialog.getContents(),
+                dialog.getCreatedDateTime().toLocalDate()
         );
     }
 }
