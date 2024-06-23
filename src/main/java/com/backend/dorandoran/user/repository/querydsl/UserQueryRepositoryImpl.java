e1bb74b15f2cd10fa96521dfc8c730bd0451e71f
dajeongdev <dajeongdev@gmail.com>
2024-06-23 18:01:09 +0900

    fix : 사용하지 않는 코드 삭제 및 상담과 대화 삭제 쿼리 수정

--- src/main/java/com/backend/dorandoran/user/repository/querydsl/UserQueryRepositoryImpl.java
+++ src/main/java/com/backend/dorandoran/user/repository/querydsl/UserQueryRepositoryImpl.java
@@ -2,17 +2,10 @@ package com.backend.dorandoran.user.repository.querydsl;
 
 import com.backend.dorandoran.counsel.domain.entity.QCounsel;
 import com.backend.dorandoran.counsel.domain.entity.QDialog;
-import com.backend.dorandoran.user.domain.entity.QUser;
-import com.backend.dorandoran.user.domain.entity.QUserToken;
-import com.backend.dorandoran.user.domain.entity.UserToken;
-import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
@@ -20,33 +13,19 @@ public class UserQueryRepositoryImpl implements UserQueryRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    private final QUser user = QUser.user;
-    private final QUserToken userToken = QUserToken.userToken;
     private final QCounsel counsel = QCounsel.counsel;
     private final QDialog dialog = QDialog.dialog;
 
     @Override
-    public Optional<UserToken> findUserTokenByPhoneNumber(String phoneNumber) {
-        return Optional.ofNullable(
-                jpaQueryFactory.select(Projections.constructor(UserToken.class,
-                            userToken.id, userToken.userId, userToken.refreshToken
-                        ))
-                        .from(user)
-                        .leftJoin(userToken).on(user.id.eq(userToken.userId))
-                        .where(user.phoneNumber.eq(phoneNumber))
-                        .fetchOne()
-        );
-    }
-
-    @Override
     public void deleteCounselAndDialogByUserId(Long userId) {
-        BooleanExpression whereClause = counsel.id.in(
-                JPAExpressions.select(dialog.counsel.id)
-                        .from(dialog)
-                        .where(dialog.counsel.user.id.eq(userId))
-        );
+        jpaQueryFactory.delete(dialog)
+                    .where(counsel.id.in(
+                            JPAExpressions.select(dialog.counsel.id)
+                                .from(dialog)
+                                .where(dialog.counsel.user.id.eq(userId)))
+                    ).execute();
         jpaQueryFactory.delete(counsel)
-                .where(whereClause)
+                .where(counsel.user.id.eq(userId))
                 .execute();
     }
 }
