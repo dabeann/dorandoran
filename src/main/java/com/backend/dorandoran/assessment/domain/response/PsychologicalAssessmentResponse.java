480de93934ba0b8e875bb8bac32b053f01ceba5b
dajeongdev <dajeongdev@gmail.com>
2024-06-24 16:25:09 +0900

    fix : 첫 심리검사 결과 API에 심리 상태 기준 추가

--- src/main/java/com/backend/dorandoran/assessment/domain/response/PsychologicalAssessmentResponse.java
+++ src/main/java/com/backend/dorandoran/assessment/domain/response/PsychologicalAssessmentResponse.java
@@ -36,23 +36,42 @@ public class PsychologicalAssessmentResponse {
     public PsychologicalAssessmentResponse(String name, LocalDateTime testDate, Integer anxiety, Integer depression, Integer stress) {
         this.name = name;
         this.testDate = testDate.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일"));
+        this.result = processAssessmentResult(anxiety, depression, stress);;
+    }
 
-        List<PsychologicalAssessmentResponse.PsychologicalAssessmentResult> results = new ArrayList<>();
-        results.add(PsychologicalAssessmentResult.builder()
-                .category(PsychologicalAssessmentCategory.ANXIETY)
-                .score(anxiety)
-                .percent(100 - anxiety)
-                .build());
+    private static List<PsychologicalAssessmentResult> processAssessmentResult(Integer anxiety, Integer depression, Integer stress) {
+        List<PsychologicalAssessmentResult> results = new ArrayList<>();
         results.add(PsychologicalAssessmentResult.builder()
                 .category(PsychologicalAssessmentCategory.DEPRESSION)
                 .score(depression)
                 .percent(100 - depression)
+                .standard(calculateStandard(100 - depression, PsychologicalAssessmentCategory.DEPRESSION))
                 .build());
-        results.add(PsychologicalAssessmentResponse.PsychologicalAssessmentResult.builder()
+        results.add(PsychologicalAssessmentResult.builder()
                 .category(PsychologicalAssessmentCategory.STRESS)
                 .score(stress)
                 .percent(100 - stress)
+                .standard(calculateStandard(100 - stress, PsychologicalAssessmentCategory.STRESS))
                 .build());
-        this.result = results;
+        results.add(PsychologicalAssessmentResult.builder()
+                .category(PsychologicalAssessmentCategory.ANXIETY)
+                .score(anxiety)
+                .percent(100 - anxiety)
+                .standard(calculateStandard(100 - anxiety, PsychologicalAssessmentCategory.ANXIETY))
+                .build());
+        return results;
+    }
+
+    private static PsychologicalAssessmentStandard calculateStandard(Integer score, PsychologicalAssessmentCategory category) {
+        int mediumMinScore = (category.equals(PsychologicalAssessmentCategory.STRESS)) ? 32 : 36;
+        int mediumMaxScore = (category.equals(PsychologicalAssessmentCategory.STRESS)) ? 80 : 75;
+
+        PsychologicalAssessmentStandard standard = PsychologicalAssessmentStandard.적음;
+        if (mediumMinScore <= score && mediumMaxScore >= score) {
+            standard = PsychologicalAssessmentStandard.중간;
+        } else if (mediumMaxScore < score) {
+            standard = PsychologicalAssessmentStandard.심각;
+        }
+        return standard;
     }
 }
